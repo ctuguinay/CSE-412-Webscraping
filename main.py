85e8fa5bb83f3b698fb2702b71cda091f0b0d55b
ctuguina <ctuguina@uw.edu>
2022-05-05 15:00:41 -0700

    further initial data collection

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
+totalData = None
+h3 = None
+first_span = None
+
 url = "https://app.leg.wa.gov/bi/report/topicalindex/?biennium=2021-22&topicId=14720"
+result = None
+while result is None:
+    try:
+        s = HTMLSession()
+        response = s.get(url)
+        response.html.render(wait=2, sleep=3)
+        soup = BeautifulSoup(response.html.html, "html.parser")
+        totalData = soup.find(id = "topicalIndexReportData")
+        h3 = totalData.find("h3")
+        first_span = h3.find("span")
+        result = True
+    except:
+         pass
 
-s = HTMLSession()
-response = s.get(url)
-response.html.render(wait=2, sleep=3)
-
-soup = BeautifulSoup(response.html.html, "html.parser")
-
-totalData = soup.find(id = "topicalIndexReportData")
-
-h3 = totalData.find("h3")
-
-first_span = h3.find("span")
+main_topic = h3.find("a").text
 
 links = []
 
@@ -22,14 +29,17 @@ for link in first_span.find_all("a"):
     href = href.replace(" ", "%20")
     links.append(href)
 
-ul = totalData.find("ul")
+ul = totalData.find("ul").find("ul")
 
 bills = []
 
 for paragraph in ul.find_all("li"):
     bill = []
+    bill.append(main_topic)
     text = paragraph.text
-    bill.append(paragraph.text)
+    text = " ".join(text.split())
+    description = text.split(":")[0]
+    bill.append(description)
     for link in paragraph.find_all("a"):
         href = link.get("href")
         bill.append(href)
