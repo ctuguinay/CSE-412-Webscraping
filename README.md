23d79f22d5a2f8f041848ba92364e1699e6099de
ctuguina <ctuguina@uw.edu>
2022-05-06 16:25:12 -0700

    additional remark

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 Description 2, Sponsors, Requester**
 
 *Note: In House, Senate, and After Passage Progress data, a 1 and 0 represent a pass or no pass in the following order:
-[Introduced, In Committee, On Floor Calendar, Passed Chamber]*
+[Introduced, In Committee, On Floor Calendar, Passed Chamber]*. Additionally, Description 1 data is bad because
+whoever inputted the data spliced and rearranged some of the sentences in the weirdest way possible.
 
 Example Row: 'COVID-19 AND CORONAVIRUS', 'Liquor licenses, businesses with, privileges granted to mitigate pandemic effects for, extending', 'https://app.leg.wa.gov/billsummary?BillNumber=1480&Year=2021&Initiative=false', 'Engrossed 2nd Substitute - E2SHB 1480', '48 L 21', ['1', '1', '1', '1'], ['1', '1', '1', '1'], ['1', '1', '1', '1'], 'Extending certain privileges granted to liquor licensees to mitigate the impact of the coronavirus pandemic.', 'MacEwen, Kloba, Sutherland, Robertson, Ormsby, Chambers, Eslick, Tharinger', 'Liquor and Cannabis Board'
--- main.py
+++ main.py
@@ -6,8 +6,9 @@ totalData = None
 h3 = None
 first_span = None
 
-topics = ["?biennium=2021-22&topicId=14720",
-    "?biennium=2021-22&topic=BUDGETS",
+#"?biennium=2021-22&topicId=14720"
+
+topics = ["?biennium=2021-22&topic=BUDGETS",
     "?biennium=2021-22&topic=COMPUTERS",
     "?biennium=2021-22&topic=EMERGENCY%20MANAGEMENT%20AND%20SERVICES",
     "?biennium=2021-22&topic=EMERGENCY,%20STATE%20OF",
@@ -27,87 +28,91 @@ topics = ["?biennium=2021-22&topicId=14720",
     "?biennium=2021-22&topic=UNEMPLOYMENT%20COMPENSATION",
     "?biennium=2021-22&topic=WORKERS%27%20COMPENSATION"]
 
-url_template = "https://app.leg.wa.gov/bi/report/topicalindex/"
 
-url = "https://app.leg.wa.gov/bi/report/topicalindex/?biennium=2021-22&topicId=14720"
-result = None
-while result is None:
-    try:
-        s = HTMLSession()
-        response = s.get(url)
-        response.html.render(wait=2, sleep=3)
-        soup = BeautifulSoup(response.html.html, "html.parser")
-        totalData = soup.find(id = "topicalIndexReportData")
-        h3 = totalData.find("h3")
-        first_span = h3.find("span")
-        result = True
-    except:
-         pass
+for index in range(len(topics)):
 
-#links = []
+    url_template = "https://app.leg.wa.gov/bi/report/topicalindex/"
+    url = url_template + topics[index]
 
-#for link in first_span.find_all("a"):
-#   href = link.get("href")
-#   href = href.replace(" ", "%20")
-#   print(href)
-#   links.append(href)
-
-main_topic = h3.find("a").text
-
-main_topic_underscore = main_topic.replace(" ", "_")
-
-with open(main_topic_underscore + ".csv", mode='w') as employee_file:
-
-    csv_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-    ul = totalData.find("ul").find("ul")
-
-    for paragraph in ul.find_all("li"):
-        bill = []
-        bill.append(main_topic)
-        text = paragraph.text
-        text = " ".join(text.split())
-        description = text.split(":")[0]
-        bill.append(description)
-        link = paragraph.find_all("a")[-1]
-        href = link.get("href")
-        bill.append(href)
-        d = HTMLSession()
-        response_d = d.get(href)
-        response_d.html.render()
-        soup_d = BeautifulSoup(response_d.html.html, "html.parser")
-        bill_name = soup_d.find_all("div", class_="billStatusAtAGlanceSubText")[0].text
-        current_status = soup_d.find_all("div", class_="billStatusAtAGlanceSubText")[1].text
-        current_status = current_status[1:]
-        current_status = current_status.strip()
-        bill.append(bill_name)
-        bill.append(current_status)
-        div = soup_d.find(id = "horizontalStatusDisplay").find("div", class_="col-xs-12")
-        svgs = div.find_all("svg")[1:4]
-        for svg in svgs:
-            circles = []
-            circle_set = svg.find_all("circle")
-            for index in range(len(circle_set)):
-                circle = circle_set[index]
-                if circle["fill"] == "white":
-                    circles.append("0")
-                elif circle["fill"] == "black":
-                    circles.append("1")
-            if len(circles) == 1:
-                circles = ["1", "1", "1", "1"]
-            bill.append(circles)
-        div = soup_d.find("div", class_="container-fluid").find("div", class_="row clearfix").findChildren("div", class_=None)[5]
-        divs = div.find_all("div", class_="row clearfix")
-        bill.append(divs[1].text.strip())
-        bill.append((" ".join(divs[2].text.split()).replace("Sponsors: ", "")))
+    result = None
+    while result is None:
         try:
-            if "Companion Bill:" in divs[3].text.strip():
-                bill.append("No Requester")
-            else:
-                bill.append(divs[3].text.strip().replace("\n", " ").replace("By Request: ", ""))
+            s = HTMLSession()
+            response = s.get(url)
+            response.html.render(wait=2, sleep=3)
+            soup = BeautifulSoup(response.html.html, "html.parser")
+            totalData = soup.find(id = "topicalIndexReportData")
+            h3 = totalData.find("h3")
+            first_span = h3.find("span")
+            result = True
         except:
-            bill.append("No Requester")
-        # Wilson, C. and Johnson, J.
-        response_d.close()
-        print(bill)
-        csv_writer.writerow(bill)
+             pass
+
+    response.close()
+    #links = []
+
+    #for link in first_span.find_all("a"):
+    #   href = link.get("href")
+    #   href = href.replace(" ", "%20")
+    #   print(href)
+    #   links.append(href)
+
+    main_topic = h3.find("a").text
+
+    main_topic_underscore = main_topic.replace(" ", "_")
+
+    with open(main_topic_underscore + ".csv", mode='w') as employee_file:
+
+        csv_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        ul = totalData.find("ul").find("ul")
+
+        for paragraph in ul.find_all("li"):
+            bill = []
+            bill.append(main_topic)
+            text = paragraph.text
+            text = " ".join(text.split())
+            description = text.split(":")[0]
+            bill.append(description)
+            link = paragraph.find_all("a")[-1]
+            href = link.get("href")
+            bill.append(href)
+            d = HTMLSession()
+            response_d = d.get(href)
+            response_d.html.render()
+            soup_d = BeautifulSoup(response_d.html.html, "html.parser")
+            bill_name = soup_d.find_all("div", class_="billStatusAtAGlanceSubText")[0].text
+            current_status = soup_d.find_all("div", class_="billStatusAtAGlanceSubText")[1].text
+            current_status = current_status[1:]
+            current_status = current_status.strip()
+            bill.append(bill_name)
+            bill.append(current_status)
+            div = soup_d.find(id = "horizontalStatusDisplay").find("div", class_="col-xs-12")
+            svgs = div.find_all("svg")[1:4]
+            for svg in svgs:
+                circles = []
+                circle_set = svg.find_all("circle")
+                for index in range(len(circle_set)):
+                    circle = circle_set[index]
+                    if circle["fill"] == "white":
+                        circles.append("0")
+                    elif circle["fill"] == "black":
+                        circles.append("1")
+                if len(circles) == 1:
+                    circles = ["1", "1", "1", "1"]
+                bill.append(circles)
+            div = soup_d.find("div", class_="container-fluid").find("div", class_="row clearfix").findChildren("div", class_=None)[5]
+            divs = div.find_all("div", class_="row clearfix")
+            bill.append(divs[1].text.strip())
+            bill.append((" ".join(divs[2].text.split()).replace("Sponsors: ", "")))
+            try:
+                if "Companion Bill:" in divs[3].text.strip():
+                    bill.append("No Requester")
+                else:
+                    bill.append(divs[3].text.strip().replace("\n", " ").replace("By Request: ", ""))
+            except:
+                bill.append("No Requester")
+            # Wilson, C. and Johnson, J.
+            response_d.close()
+            print(bill)
+            csv_writer.writerow(bill)
