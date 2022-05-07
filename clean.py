921b552bcfdb03474ffb2b864f77bcf194b3715d
ctuguina <ctuguina@uw.edu>
2022-05-06 21:05:06 -0700

    clean csvs

---
+++ clean.py
@@ -0,0 +1,20 @@
+from tempfile import NamedTemporaryFile
+from os import listdir
+import shutil
+import csv
+
+topics = listdir("topic_CSVS")
+
+for topic in topics:
+    filename = "topic_CSVS/" + topic
+    tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
+
+    with open(filename, 'r', newline='') as csvFile, tempfile:
+        reader = csv.reader(csvFile, delimiter=',', quotechar='"')
+        writer = csv.writer(tempfile, delimiter=',', quotechar='"')
+
+        for row in reader:
+            if len(row) == 11:
+                writer.writerow(row)
+
+    shutil.move(tempfile.name, filename)
