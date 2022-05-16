dc8683da98e1f69a575d84f25c03c8939dcf95e5
ctuguina <ctuguina@uw.edu>
2022-05-16 15:54:43 -0700

    bill or not csv add

--- status_simplify.py
+++ status_simplify.py
@@ -8,6 +8,7 @@ with open("second_revised_CSVs/status_simplify.csv", mode='w', newline='') as fi
         if not row[0] == "bill_number":
             simplified_status = ""
             current_chamber = ""
+            bill = "no"
             status = row[3]
             if status[0] == "H":
                 current_chamber = "Representatives"
@@ -17,6 +18,7 @@ with open("second_revised_CSVs/status_simplify.csv", mode='w', newline='') as fi
                 current_chamber = "Neither"
             if "C" == status[0]:
                 simplified_status = "Law"
+                bill = "yes"
             elif "Gov vetoed" in status:
                 simplified_status = "Governor Veto"
             elif "Rules X" in status:
@@ -27,10 +29,11 @@ with open("second_revised_CSVs/status_simplify.csv", mode='w', newline='') as fi
                 simplified_status = "Non-Rules Committee"
             row.append(simplified_status)
             row.append(current_chamber)
+            row.append(bill)
             writer.writerow(row)
         else:
             columns = ["bill_number", "bill_link", "bill_name", "current_status",
                        "first_house_progress", "second_house_progress", "after_passage_progress", "description",
                        "sponsors", "requester", "bill_history", "democrat_sponsor_count", "republican_sponsor_count",
-                       "total_days_spent", "season_years_active", "simplified_status", "current_chamber"]
+                       "total_days_spent", "season_years_active", "simplified_status", "current_chamber", "bill"]
             writer.writerow(columns)
