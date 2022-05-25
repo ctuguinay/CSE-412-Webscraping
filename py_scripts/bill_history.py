20ae9d86f325b40d733b3acc66fe08cbe6acae64
ctuguina <ctuguina@uw.edu>
2022-05-24 21:47:33 -0700

    example bill history data in csv

--- py_scripts/bill_history.py
+++ py_scripts/bill_history.py
@@ -4,55 +4,72 @@ import csv
 
 with HTMLSession() as session:
 
-    with open("bill_history_CSVs/bill_history.csv", mode='w') as file:
+    with open("bill_history_CSVs/bill_history.csv", mode='w', newline='') as file:
 
         csv_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-        for number in list(range(1000, 4000)) + list(range(5000, 8000)):
+        url = "https://app.leg.wa.gov/billsummary?BillNumber=1162&Year=2021&Initiative=false"
 
-            url = "https://app.leg.wa.gov/billsummary?BillNumber=" + str(number) + "&Year=2021&Initiative=false"
+        bill = [1162, url]
 
-            bill = [number, url]
+        response = session.get(url)
+        response.html.render()
+        soup = BeautifulSoup(response.html.html, "html.parser")
 
-            response = session.get(url)
-            response.html.render()
-            soup = BeautifulSoup(response.html.html, "html.parser")
-
-            clearfix_rows = soup.find(id="pageContent").find_all("div", class_="row clearfix")
-            count = 0
-            for row in clearfix_rows:
-                if "Bill Not Found." in row.text:
-                    count = count + 1
-
-            if not count > 0:
-                current_status = soup.find_all("div", class_="billStatusAtAGlanceSubText")[1].text
-                current_status = current_status.strip()
-                bill.append(current_status)
-                div = soup.find("div", class_="container-fluid").find("div", class_="row clearfix").findChildren("div",class_=None)[5]
-                divs = div.find_all("div", class_="row clearfix")
-                bill_history = soup.find("div", class_="col-xs-12 col-csm-11 col-sm-10 col-md-9 col-lg-10")
-                p_list = bill_history.find_all("p")
-                history_table_list = bill_history.find_all("div", class_="historytable")
-                string = ""
-                for index in range(len(p_list)):
-                    p = p_list[index]
-                    step = p.text.strip()
-                    history_table = history_table_list[index]
-                    dates = history_table.find_all("div", class_="historyrow")
-                    first_date = dates[0].text
-                    last_date = ""
-                    count = 1
-                    while True:
-                        if not dates[len(dates) - count].text.strip():
-                            count = count + 1
+        clearfix_rows = soup.find(id="pageContent").find_all("div", class_="row clearfix")
+        count = 0
+        for row in clearfix_rows:
+            if "Bill Not Found." in row.text:
+                count = count + 1
+        if not count > 0:
+            bill_name = soup.find_all("div", class_="billStatusAtAGlanceSubText")[0].text
+            current_status = soup.find_all("div", class_="billStatusAtAGlanceSubText")[1].text
+            current_status = current_status.strip()
+            bill.append(bill_name)
+            bill.append(current_status)
+            div = soup.find("div", class_="container-fluid").find("div", class_="row clearfix").findChildren("div",class_=None)[5]
+            divs = div.find_all("div", class_="row clearfix")
+            bill_history = soup.find("div", class_="col-xs-12 col-csm-11 col-sm-10 col-md-9 col-lg-10")
+            p_list = bill_history.find_all("p")
+            history_table_list = bill_history.find_all("div", class_="historytable")
+            string = ""
+            csv_writer.writerow(bill)
+            cur_year = "2021"
+            for index in range(len(p_list)):
+                total = []
+                p = p_list[index]
+                step = p.text.strip()
+                if "2022" in step:
+                    cur_year = "2022"
+                history_table = history_table_list[index]
+                dates = history_table.find_all("div", class_="historyrow")
+                texts = history_table.find_all("div", attrs={'style': 'display: table-cell; padding-right: 0.5em;'})
+                count = 0
+                for i in range(len(texts)):
+                    text = texts[i].text.split("(")[0].strip()
+                    date = dates[i].text.strip()
+                    combine = [date, text]
+                    total.append(combine)
+                revised_total = []
+                for i in range(len(total)):
+                    cur_date = total[i][0]
+                    cur_text = total[i][1]
+                    if len(cur_date) == 0:
+                        revised_total[len(revised_total) - 1][1] = revised_total[len(revised_total) - 1][1] + " " + cur_text
+                    else:
+                        revised_total.append(total[i])
+                for i in range(len(revised_total)):
+                    date = revised_total[i][0]
+                    text = revised_total[i][1]
+                    revised_total[i][1] = " ".join(text.split())
+                    if "Dec" in date:
+                        if cur_year == "2021":
+                            date = date + " 2020"
                         else:
-                            last_date = dates[len(dates) - count].text.strip()
-                            break
-                    first_date = first_date.strip()
-                    last_date = last_date.strip()
-                    string = string + step + ": Start " + first_date + " End " + last_date + ". "
-                string = string.strip()
-                bill.append(string)
-                response.close()
-                print(bill)
-                #csv_writer.writerow(bill)
+                            date = date + " 2021"
+                    else:
+                        date = date + " " + cur_year
+                    revised_total[i][0] = date
+                for i in range(len(revised_total)):
+                    csv_writer.writerow([step] + revised_total[i])
+            response.close()
